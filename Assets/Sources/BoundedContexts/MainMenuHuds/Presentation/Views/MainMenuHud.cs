7a1904c26d60e40e463d30a00ff659f7d1421b85
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-07-22 11:48:54 +0300

    add ui for main menu form

--- Assets/Sources/BoundedContexts/MainMenuHuds/Presentation/Views/MainMenuHud.cs
+++ Assets/Sources/BoundedContexts/MainMenuHuds/Presentation/Views/MainMenuHud.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using Sources.BoundedContexts.MainMenuForms.Presentation.Implementation;
+using UnityEngine;
 
 namespace Sources.BoundedContexts.MainMenuHuds.Presentation.Views
 {
     public class MainMenuHud : MonoBehaviour
     {
+        [SerializeField] private MainMenuForm _mainMenuForm;
         
+        public MainMenuForm MainMenuForm => _mainMenuForm;
     }
 }
